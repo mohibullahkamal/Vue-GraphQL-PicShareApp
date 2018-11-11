4519dc945612b20df292c68b89d5eadfbcd6bdc3
mogit2020 <mohibullah.kamal@gmail.com>
2018-11-11 20:01:04 +0100

    Innitialize Apollo server

---
+++ server.js
@@ -0,0 +1,5 @@
+const { ApolloServer } = require('apollo-server');
+
+const server = new ApolloServer({}); //We innitialize Apollo server here
+
+server.listen; //now that we have innitialised Apollo server; we can call a method on it
