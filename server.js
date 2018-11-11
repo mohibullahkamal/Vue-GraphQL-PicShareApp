31e716ed7ae879da9bae9c03cf660c69eee12cf4
mogit2020 <mohibullah.kamal@gmail.com>
2018-11-11 21:14:03 +0100

    Update bugs

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
 const { ApolloServer } = require('apollo-server');
 
-const server = new ApolloServer({}); //We innitialize Apollo server here
+const todos = [
+  { task: 'Wash car', completed: false }, //personal added todos
+  { task: 'Clean room', completed: true } //manual data added
+];
 
-server.listen; //now that we have innitialised Apollo server; we can call a method on it
+const typeDefs = gql, //we tell our data
+
+
+type Todo {
+    task: String,
+    completed: Boolean
+}
+
+
+const server = new ApolloServer({
+    typeDefs: typek
+}); //We innitialize Apollo server here
+
+server.listen().then(({ url } ) => {
+    console.log('Server listening');
+}); //now that we have innitialised Apollo server; we can call a method on Server
