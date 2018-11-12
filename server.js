ff8845d969f2a6c977328cc9453a701a7f9c34ac
mogit2020 <mohibullah.kamal@gmail.com>
2018-11-12 11:37:45 +0100

    Define type definitions

--- server.js
+++ server.js
@@ -1,52 +1,22 @@
-const { ApolloServer } = require('apollo-server');
+const { ApolloServer, gql } = require('apollo-server');
 
 const todos = [
   { task: 'Wash car', completed: false }, //personal added todos
   { task: 'Clean room', completed: true } //manual data added
 ];
 
-const typeDefs = gql, //we tell our data
-
-
-type Todo {
-    task: String,
+//With in back-ticks we want to define type definitions
+const typeDefs = gql`
+  type Todo {
+    task: String
     completed: Boolean
-}
+  }
+`;
 
-//********** */
-// ***********
 const server = new ApolloServer({
-    typeDefs: typek
+  typeDefs: typek
 }); //We innitialize Apollo server here
 
-server.listen(4000).then(({ url } ) => {
-    console.log('Server listening');
-}); //now that we have innitialised Apollo server; we can call a method on Server
-
-
-
-
-
-
-
-
-
-//********** */
-// ***********
-const server = new ApolloServer({
-    typeDefs: typek
-}); //We innitialize Apollo server here
-
-server.listen(4000).then(({ url } ) => {
-    console.log('Server listening');
-}); //now that we have innitialised Apollo server; we can call a method on Server
-
-//********** */
-// ***********
-const server = new ApolloServer({
-    typeDefs: typek
-}); //We innitialize Apollo server here
-
-server.listen(4000).then(({ url } ) => {
-    console.log('Server listening');
+server.listen(4000).then(({ url }) => {
+  console.log('Server listening');
 }); //now that we have innitialised Apollo server; we can call a method on Server
