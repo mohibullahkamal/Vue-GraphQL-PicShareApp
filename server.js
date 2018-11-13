181f835dbf9050dd73f0f6ff51bf3a8627d6f0a6
mogit2020 <mohibullah.kamal@gmail.com>
2018-11-13 14:45:16 +0100

    Create Server.js

--- server.js
+++ server.js
@@ -11,12 +11,34 @@ const typeDefs = gql`
     task: String
     completed: Boolean
   }
+
+  type Query {
+    getTodos: [Todo]
+  }
+
+  type Mutation {
+    addTodo(task: String, completed: Boolean): Todo
+  }
 `;
 
+const resolvers = {
+  Query: {
+    getTodos: () => todos
+  },
+  Mutation: {
+    addTodo: (_, { task, completed }) => {
+      const todo = { task, completed };
+      todos.push(todo);
+      return todo;
+    }
+  }
+};
+
 const server = new ApolloServer({
-  typeDefs: typek
+  typeDefs,
+  resolvers
 }); //We innitialize Apollo server here
 
-server.listen(4000).then(({ url }) => {
-  console.log('Server listening');
+server.listen().then(({ url }) => {
+  console.log(`Server listening on ${url}`);
 }); //now that we have innitialised Apollo server; we can call a method on Server
