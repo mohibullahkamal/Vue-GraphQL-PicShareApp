f741420da496659b33e4178fc7ee6212b8cfc78f
mogit2020 <mohibullah.kamal@gmail.com>
2018-11-18 18:03:06 +0100

    DB connected

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose');
 require('dotenv').config({ path: 'variable.env' });
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(
+    process.env.MONGO_URI,
+    { useNewUrlParser: true }
+  )
   .then(() => console.log('DB connected'))
   .catch(err => console.error(err));
 
