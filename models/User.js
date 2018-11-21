9dbac84db9b3235e6e931256e45bbfd31511cce1
mogit2020 <mohibullah.kamal@gmail.com>
2018-11-21 20:54:28 +0100

    Update Mongoose Schema

--- models/User.js
+++ models/User.js
@@ -1,9 +1,15 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
+  username: {
+      type: String,
+      
+  }
+
   username: {},
   email: {},
   password: {},
   avatar: {},
-  joinDate: {}
+  joinDate: {}, 
+  favorites: {}
 });
